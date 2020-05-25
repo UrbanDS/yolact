5fa6515bb888ca396756cb7f66f49215e1d938fb
dc <divya.chandana@gmail.com>
2020-05-25 00:03:21 -0400

    default name changed

--- eval.py
+++ eval.py
@@ -41,7 +41,7 @@ def parse_args(argv=None):
     parser = argparse.ArgumentParser(
         description='YOLACT COCO Evaluation')
     parser.add_argument('--trained_model',
-                        default='weights/yolact_resnet50_cig_butts_config.pth', type=str,
+                        default='weights/yolact_plus_resnet50_cig_butts_25_453_interrupt.pth', type=str,
                         help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
     parser.add_argument('--top_k', default=5, type=int,
                         help='Further restrict the number of predictions to parse')
